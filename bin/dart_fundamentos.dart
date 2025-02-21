8ae2e9d423e18131bc24a9a17339501c78acc38e
rhayssam <rhayssam@crmsolucoes.com.br>
2025-02-21 08:30:17 -0300

    feat: subindo projeto

---
+++ bin/dart_fundamentos.dart
@@ -0,0 +1,3 @@
+import 'package:dart_fundamentos/dart_fundamentos.dart' as dart_fundamentos;
+
+void main(List<String> arguments) {}
---
+++ lib/1_variaveis/1_variaveis.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Declaração de variáveis
+  // int, double, String, bool
+  int idade = 30;
+  double altura = 1.80;
+  String nome = "João";
+  bool fumante = false;
+
+  // Variáveis sem tipo definido
+  var valor = 10;
+  var peso = 80.5;
+  var texto = "Olá";
+  var chove = true;
+
+  // Variáveis dinâmicas
+  dynamic valorDinamico = 10;
+  valorDinamico = "Dez";
+  print(valorDinamico);
+
+  // Constantes
+  const pi = 3.14;
+  final gravidade = 9.8;
+
+  // Constantes em tempo de compilação
+  var tempoDeCompilacao = DateTime.now();
+  print(tempoDeCompilacao);
+
+  // Constantes em tempo de execução
+  final tempoDeExecucao = DateTime.now();
+  print(tempoDeExecucao);
+}
