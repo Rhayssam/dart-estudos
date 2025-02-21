7ba2021f91d66c72233065351c7afcf1315ccf59
rhayssam <rhayssam@crmsolucoes.com.br>
2025-02-21 11:24:14 -0300

    feat: adicionando os modificadores (const e final)

---
+++ lib/2_modificadores/1_modificadores.dart
@@ -0,0 +1,14 @@
+void main() {
+  var nomeCompleto = 'Rhayssam';
+  print(nomeCompleto);
+
+  nomeCompleto = 'Rhayssam Exemplo';
+  print(nomeCompleto);
+
+  // Final - em tempo de execução
+  final nomeCompletoFinal = 'Rhayssam Exemplo Final';
+  print(nomeCompletoFinal);
+  // Const - em tempo de compilação
+  const nomeCompletoConst = 'Rhayssam Exemplo Const';
+  print(nomeCompletoConst);
+}
